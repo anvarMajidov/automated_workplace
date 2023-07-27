c827fcd51ba5605d3d1a3d727188c040821c5217
AMazhidov <majidov3011@gmail.com>
2023-07-27 19:22:51 +0500

    Register DbContext in API Services

---
+++ src/backend/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DbConnection"));
+        });
+        return services;
+    }
+}
