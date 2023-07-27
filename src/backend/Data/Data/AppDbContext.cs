d62994a789db574eb475d92e0620812f8014b3d9
AMazhidov <majidov3011@gmail.com>
2023-07-27 19:22:24 +0500

    Add DbContext class

---
+++ src/backend/Data/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Models.DbModels;
+
+namespace Data.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<CarModel> CarModels { get; set; }
+    public DbSet<CarBrand> CarBrands { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Master> Masters { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
