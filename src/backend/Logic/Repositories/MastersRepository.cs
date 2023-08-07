ac99c3b5a97f428b0fdef069e66e273d419a8cc2
AMazhidov <majidov3011@gmail.com>
2023-08-07 21:44:58 +0500

    Add MastersRepository

---
+++ src/backend/Logic/Repositories/MastersRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.Data;
+using Data.Repositories;
+using Models.DbModels;
+
+namespace Logic.Repositories;
+
+public class MastersRepository : BaseRepository<Master>, IMastersRepository
+{
+    private readonly AppDbContext _context;
+    
+    public MastersRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
