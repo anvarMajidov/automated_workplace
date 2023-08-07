051409c1273e2068bf035394ec221471872e402e
AMazhidov <majidov3011@gmail.com>
2023-08-07 21:44:43 +0500

    Implement unit of work pattern

---
+++ src/backend/Data/Data/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Data.Repositories;
+
+namespace Data.Data;
+
+public interface IUnitOfWork
+{
+    public IClientsRepository ClientsRepository { get; }
+    public ICarsRepository CarsRepository { get; }
+    public IMastersRepository MasterRepository { get; }
+    public ICarModelsRepository CarModelsRepository { get; }
+    public ICarBrandsRepository CarBrandsRepository { get; }
+
+    public bool SaveChanges();
+    public Task<bool> SaveChangesAsync();
+}
