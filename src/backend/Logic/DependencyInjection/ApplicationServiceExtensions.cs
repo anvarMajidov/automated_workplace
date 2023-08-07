38c32643038d860b4ff8d64af3e9ee57bb8e7ba8
AMazhidov <majidov3011@gmail.com>
2023-08-07 21:46:52 +0500

    Configure mappings Automapper

---
+++ src/backend/Logic/DependencyInjection/ApplicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Data.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logic.DependencyInjection;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DbConnection"));
+        });
+        return services;
+    }
+    public static IServiceCollection AddAutomapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+}
---
+++ src/backend/Logic/Profiles/AutomapperProfiles.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Models.DbModels;
+using Models.DTOs.CarBrandDTOs;
+using Models.DTOs.CarDTOs;
+using Models.DTOs.CarModelDTOs;
+using Models.DTOs.ClientDTOs;
+
+namespace Logic.Profiles;
+
+public class AutomapperProfiles : Profile
+{
+    public AutomapperProfiles()
+    {
+        //mappings for car entity
+        CreateMap<Car, CarDto>()
+            .ForMember(d => d.CarModelName, o =>
+                o.MapFrom(c => c.CarModel.Name))
+            .ForMember(d => d.CarBrandName, o =>
+                o.MapFrom(c => c.CarBrand.Name));
+        CreateMap<AddCarDto, Car>();
+        CreateMap<UpdateCarDto, Car>();
+
+        //mappings for client entity
+        CreateMap<AddClientDto, Client>();
+        CreateMap<UpdateClientDto, Client>();
+        CreateMap<Client, ClientDto>();
+        
+        //mappings for CarBrand entity
+        CreateMap<AddCarBrandDto, CarBrand>();
+        CreateMap<UpdateCarBrandDto, CarBrand>();
+        CreateMap<CarBrand, CarBrandDto>();
+        
+        //mappings for CarModel entity
+        CreateMap<AddCarModelDto, CarModel>();
+        CreateMap<CarModel, CarModelDto>();
+    }
+}
