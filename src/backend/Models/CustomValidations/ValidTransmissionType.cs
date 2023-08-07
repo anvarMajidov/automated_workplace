da4b5826106d54554f2fa74daeeea13ba49144c8
AMazhidov <majidov3011@gmail.com>
2023-08-07 21:43:04 +0500

    Add Custom Validator for TransmissionType

---
+++ src/backend/Models/CustomValidations/ValidTransmissionType.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Models.Enums;
+
+namespace Models.CustomValidations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class ValidTransmissionType : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is TransmissionType transmissionType)
+        {
+            if (transmissionType != TransmissionType.Mechanics &&
+                transmissionType != TransmissionType.Auto &&
+                transmissionType != TransmissionType.Variator)
+            {
+                return new ValidationResult("Invalid TransmissionType. The value should be 1, 2, or 3.");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
